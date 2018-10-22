b2870164365f34000bcede6062b98c7f2e590dc0
Bernhard Riegler <b.riegler@gentics.com>
2018-10-22 10:12:17 +0200

    cleanup

--- portal/resources/views/contentpage.blade.php
+++ portal/resources/views/contentpage.blade.php
@@ -3,9 +3,10 @@
 @section('title', $node['fields']['title'])
 
 @section('content')
-    <div class="jumbotron">
-        <h1 class="display-4">Gentics Mesh Demo Application</h1>
-        <a class="btn" href="https://getmesh.io/docs/beta/getting-started.html" role="button">Learn more</a>
-    </div>
+<div class="jumbotron">
+    <h1 class="display-4">Gentics Mesh Demo Application</h1>
+    <h2>{{$node['fields']['name']}}</h2>
+    <a class="btn" href="https://getmesh.io/docs/beta/getting-started.html" role="button">Learn more</a>
+</div>
 @endsection
 
