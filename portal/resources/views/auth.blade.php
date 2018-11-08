d662c95eb1a3efebb5698674c743830e6db803f9
Péter Nyári <p.nyari@gentics.com>
2018-11-08 16:24:06 +0100

    Switch to username/password auth (PoC)

--- portal/resources/views/auth.blade.php
+++ portal/resources/views/auth.blade.php
@@ -35,12 +35,16 @@
             </div>
         </div>
     </div>
-    <!-- SIGN IN BUTTON -->
-    {{--<button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter1">
+
+    <!-- SIGN IN BUTTON WITH MODAL -->
+    <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#exampleModalCenter1">
         Sign In
     </button>
-    --}}
+
+    {{--
+    <!-- SIGN IN BUTTON WITH REDIRECT -->
     <a href="{{ route('login', ['returnTo' => url()->full()]) }}" class="btn btn-dark">Sign in</a>
+    --}}
 
     <!-- SIGN IN MODAL -->
     <div class="modal fade" id="exampleModalCenter1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -48,12 +52,12 @@
             <div class="modal-content">
                 <div class="modal-body">
                     <h5 class="modal-title" id="exampleModalLongTitle">Great to have you back!</h5>
-                    <form>
+                    <form method="post" action="{{ route('loginPassword', ['returnTo' => url()->full()]) }}">
                         <div class="form-group">
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="username">
+                            <input type="text" name="username" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="username">
                         </div>
                         <div class="form-group">
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="password">
+                            <input type="password" name="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="password">
                             <a href="#" id="resetBtn">Reset</a></small>
                         </div>
                         <button type="submit" class="btn btn-secondary submitBtn">Sign in to your account</button>
