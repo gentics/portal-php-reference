55b989e028944bab1d14b9809a04b73ee4f698eb
Bernhard Riegler <b.riegler@gentics.com>
2019-01-08 15:42:29 +0100

    fix breadcrumbtemplate in template in preview

--- portal/resources/views/common/breadcrumb.blade.php
+++ portal/resources/views/common/breadcrumb.blade.php
@@ -5,7 +5,7 @@
         </li>
         @if(isset($node['breadcrumb']))
             @foreach (array_reverse($node['breadcrumb']) as $breadcrumb)
-                @if(@isset($breadcrumb['node']['fields']['startpage']['path']) && $node['node']['path'] != $breadcrumb['node']['fields']['startpage']['path'] && !$loop->last)
+                @if(@isset($breadcrumb['node']['fields']['startpage']['path']) && @isset($node['node']['path']) && $node['node']['path'] != $breadcrumb['node']['fields']['startpage']['path'] && !$loop->last)
                     <li class="breadcrumb-item">
                         <a href="{{ $breadcrumb['node']['fields']['startpage']['path'] }}">
                             {{ $breadcrumb['node']['fields']['name'] }}
