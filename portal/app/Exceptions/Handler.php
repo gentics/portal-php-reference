a7c1c79b4072965e9e9303c7faa0acfbb7203ff8
Péter Nyári <p.nyari@gentics.com>
2019-02-21 08:42:49 +0100

    Replace error handler to default as the library will take care of this

--- portal/app/Exceptions/Handler.php
+++ portal/app/Exceptions/Handler.php
@@ -2,11 +2,13 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
 /**
  * Class Handler
  * @package App\Exceptions
  */
-class Handler extends \Gentics\PortalPhp\Exceptions\Handler
+class Handler extends ExceptionHandler
 {
     /**
      * A list of the exception types that are not reported.
