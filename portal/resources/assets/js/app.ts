f98680e190d94499eb6f3bd7c55138d168a2f276
Péter Nyári <p.nyari@gentics.com>
2019-09-17 13:40:22 +0200

    Add Vue back

--- portal/resources/assets/js/app.ts
+++ portal/resources/assets/js/app.ts
@@ -1,8 +1,6 @@
 // Initialize Legacy code, refactored to TypeScript
 import { Legacy } from './legacy';
 
-new Legacy();
-
 // Initialize Vue Components
 import Vue from "vue";
 import RatingComponent from "./components/RatingComponent.vue";
@@ -11,6 +9,11 @@ import RatingComponent from "./components/RatingComponent.vue";
 Vue.component('rating', RatingComponent);
 
 // Start Vue
-/*new Vue({
-	el: '#app'
-});*/
+new Vue({
+	el: '#app',
+    mounted: () => {
+	    // Load legacy code after Vue initialized
+        // Because some event listeners need to be created
+        new Legacy();
+    }
+});
