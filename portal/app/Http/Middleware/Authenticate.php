698f4713f11e1ce065adf60f5c25b58909de2dc9
Péter Nyári <p.nyari@gentics.com>
2019-11-18 16:05:00 +0100

    Add dummy authenticate middlware

--- portal/app/Http/Middleware/Authenticate.php
+++ portal/app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Gentics\PortalPhp\Features\Keycloak\Http\Middleware\Authenticate as Middleware;
 
+if (app()->bound())
 class Authenticate extends Middleware
 {
 }
